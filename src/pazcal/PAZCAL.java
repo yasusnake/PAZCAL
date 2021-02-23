83d741daed2eedc40437a2d938a301a04b3f80e0
Chris Baltar <ihshinron1@gmail.com>
2021-02-22 21:44:11 -0600

    Generacion del archivo de errores

--- src/pazcal/PAZCAL.java
+++ src/pazcal/PAZCAL.java
@@ -3,11 +3,14 @@ package pazcal;
 import java.util.StringTokenizer;
 //import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PAZCAL {
     private static String muestraContenido(String archivo) throws FileNotFoundException, IOException {
@@ -30,14 +33,22 @@ public class PAZCAL {
     
     private static String getFileExtension(File file) {
         String extension = "";
+        
+        System.out.println("Analizando la extensión del archivo");
  
         try {
             if (file != null && file.exists()) {
                 String name = file.getName();
+                String fileName = name.replaceFirst("[.][^.]+$", "");
                 extension = name.substring(name.lastIndexOf("."));
+                extension = extension.toLowerCase();
+                
+                if(extension.equals(".pazcal"))
+                    creandoArchivodeErrores(fileName);
             }
         } catch (Exception e) {
             extension = "";
+            System.err.println(e);
         }
  
         return extension;
@@ -52,8 +63,27 @@ public class PAZCAL {
         
 //        return temp;
     }
+    
+    public static void creandoArchivodeErrores(String nombre) {
+        System.out.println("Creando archivo de errores");
+        try {
+            String ruta = nombre + "-errores.txt";
+            File file = new File(ruta);
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e);
+        }
+    }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        System.out.println("Lectura del archivo");
         String ruta = "INFLACION.pazcal";
 //        String ruta = args[0];
         
@@ -64,7 +94,8 @@ public class PAZCAL {
         
         if(extension.equals(".pazcal")) {
             String pazcal = muestraContenido(ruta);
-            analizador(pazcal);
+            
+//            analizador(pazcal);
             
 //            System.out.println(pazcal);
         } else {
